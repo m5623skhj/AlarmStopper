300d567323004636497111c659bd5f00ce07719b
m5623skhj <m5623skhj@gmail.com>
2023-11-04 19:35:41 +0900

    * 버튼 클릭 확인 커밋

--- app/src/main/java/com/example/alarmstopper/MainActivity.kt
+++ app/src/main/java/com/example/alarmstopper/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.alarmstopper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val stopButton = findViewById<Button>(R.id.stopAlarmButton)
+        stopButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "stopAlarmButton",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val addDeviceButton = findViewById<Button>(R.id.addDivceButton)
+        addDeviceButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "addDeviceButton",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
