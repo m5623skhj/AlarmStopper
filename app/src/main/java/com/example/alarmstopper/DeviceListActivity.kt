3787c64caa33e0aaeadb0baca7dedcde9db9d0ae
m5623skhj <m5623skhj@gmail.com>
2023-11-06 23:50:19 +0900

    * 메인 버튼들 위치가 이상했던 것 수정 ** 왜 그렇게 변경 됐는지는 모르겠음; * 장치 추가 페이지에서 메인 페이지로 이동할 수 있도록 버튼 추가 * 왜인지는 모르겠는데 앱을 실행하면 바로 튕김 ** sun.nio.fs.unixexception no such file or directory 라는 에러가 발생하는데 뭔지 모르겠음;

--- app/src/main/java/com/example/alarmstopper/DeviceListActivity.kt
+++ app/src/main/java/com/example/alarmstopper/DeviceListActivity.kt
@@ -1,11 +1,22 @@
 package com.example.alarmstopper
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DeviceListActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_list)
+
+        val goToMainButton = findViewById<Button>(R.id.addDivceButton)
+        goToMainButton.setOnClickListener {
+            val mainActivity = Intent(this, MainActivity::class.java)
+            startActivity(mainActivity)
+            finish()
+        }
     }
 }
