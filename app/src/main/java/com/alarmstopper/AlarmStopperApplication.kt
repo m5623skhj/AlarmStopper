183bbbf4f8005d8d72933192df1a489458b5c343
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-11-08 23:30:59 +0900

    * 잘못 기입된 파일 수정

--- app/src/main/java/com/alarmstopper/AlarmStopperApplication.kt
+++ app/src/main/java/com/alarmstopper/AlarmStopperApplication.kt
@@ -12,11 +12,48 @@ import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class AlarmStopperApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        }
+
+        FirebaseApp.initializeApp(this)
+
+        startKoin {
+            androidLogger()
+            androidContext(this@AlarmStopperApplication)
+            modules(appModule)
+        }
+
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NotificationManager::class.java)
+
+            val alarmChannel = NotificationChannel(
+                "alarm_channel",
+                "알람",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "알람 알림"
+                setSound(null, null)
+            }
+
+            val syncChannel = NotificationChannel(
+                "sync_channel",
+                "동기화",
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                description = "디바이스 간 동기화"
+            }
+
+            notificationManager.createNotificationChannel(alarmChannel)
+            notificationManager.createNotificationChannel(syncChannel)
         }
     }
 }
